bc89542c971199431886db4084fa271f7597fa26
Ali <lalsdysy@gmail.com>
2026-04-17 04:03:08 +0300

    add pillow dependency

--- barcodes/settings.py
+++ barcodes/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e0udpb!4z=)e77829b0$greapax$p-q6%!d1s01xal4ewj2fat'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['barcode-generator-26jx.onrender.com']
 
 
 # Application definition
